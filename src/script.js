b102885c27c3c8b7d1e260be4fb9bc272cf3a550
catb0591 <79432539+catb0591@users.noreply.github.com>
2023-04-17 20:42:18 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-let passwordInput = document.getElementById("password")
+let passwordInput = document.getElementById("password") //instead of having to put .value in every line of this code that references passwordInput you could just add.value to the end of this line and therefore the variable already has it built in
 
 function checkCorrectPassword() {
 
@@ -10,6 +10,18 @@ function checkCorrectPassword() {
         alert("Password requires at least 1 lowercase letter!");
     }
 
+    /*to be more efficient have the if else be searching for the opposite 
+    Example:
+    
+    let lowerCaseLetters = /[a-z]/;
+    if (!passwordInput.value.match(lowerCaseLetters)) {
+        alert("Password requires at least 1 lowercase letter!");
+    }
+    
+    You can then do this to each if else block to make it more efficient and not having the empty logic withint he if statement. 
+    */
+    
+    
     //check for capital letters
     let upperCaseLetters = /[A-Z]/;
     if (passwordInput.value.match(upperCaseLetters)){
@@ -41,3 +53,5 @@ function checkCorrectPassword() {
         alert("Password accepted!")
     }
 }
+
+//^^^this last else statement would still hold true
